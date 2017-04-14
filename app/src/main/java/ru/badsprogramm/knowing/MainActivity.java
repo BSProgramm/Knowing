4179ebda58c72c42bb8b2f928978e2fd58a868d4
BSProgramm <badsprogramm@gmail.com>
2017-04-14 12:43:24 +0300

    Initial commit

---
+++ app/src/main/java/ru/badsprogramm/knowing/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.badsprogramm.knowing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
